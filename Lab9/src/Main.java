14ade9a353c64e1d454d8018fbb71570080d73f7
Thomas <thomas.powis@maine.edu>
2023-04-05 11:36:49 -0400

    Merging Nikki's complete methods

--- Lab9/src/Main.java
+++ Lab9/src/Main.java
@@ -7,8 +7,76 @@ import student.Song;
 
 public class Main {
 
+	//Helper method for Task 2
+	public static TreeMap<String,Integer> rank(TreeMap<String,TreeSet<String>> s)
+	{
+		TreeMap<String,Integer> ranks=new TreeMap<String,Integer>();
+		for(String student:s.keySet())
+		{
+			for(String movie:s.get(student))
+			{
+				if(!ranks.containsKey(movie))
+				{
+					ranks.put(movie, 1);
+				}
+				else 
+				{
+					ranks.put(movie, ranks.get(movie)+1);
+				}
+			}
+		}
+		return ranks;
+	}
+	
+	//Helper method for Task 2
+	public static boolean contains(String[] S,String target)
+	{
+		for(String s:S)
+		{
+			if(s==target)
+			{
+
+				return true;		
+			}
+				
+		}
+		return false;
+	}
+	
+	//Task 2: Ranking the movies
+	//Programmed by Nikki
+	public static String MostPopular(TreeMap<String,TreeSet<String>> s,String[] students)
+	{
+		String max="";
+		if(s.keySet().size()>0)
+		{
+		TreeMap<String,TreeSet<String>> currstudents=new TreeMap<String,TreeSet<String>>();
+		for(String tempstudent:s.keySet())
+		{
+			if(contains(students,tempstudent))
+			{
+				currstudents.put(tempstudent, s.get(tempstudent));
+			}
+		}
+		
+		TreeMap<String,Integer> rankings=rank(currstudents);
+		max=rankings.firstKey();
+		for(String test:rankings.keySet())
+		{
+			if(rankings.get(test)>rankings.get(max))
+			{
+				max=test;
+			}
+		}
+		}
+		return max;
+	}
+	
+	
 	//Task 3: Testing Method
 	//Programmed by Thomas Powis
+	//Todo: 1.) Add Nikkis method 2.) Finish and commit the text files 3.) Make a implementation method 
+	//for testing
 	public static void test() {
 		
 		//First create the ten test cases this is going to be 10 text files
@@ -33,9 +101,9 @@ public class Main {
 			//Implementation method takes in array of student names and TreeMap
 			
 			//Next pass the TreeMap and Array into the implementation method to get the movie to watch
-			String result = rank(studentNames, temp);
+			String result = MostPopular(temp, studentNames);
 			
-			System.out.println("Methods answer: " + result);
+			System.out.println("Methods Answer: " + result);
 			System.out.println("Correct Answer: " + answers[i]);
 		}
 		
